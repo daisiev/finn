f184cb3090a66f79fa455fe20f2fdb99c5ed0d96
River <river@Radi-n-Riv.local>
2019-09-14 11:14:40 -0700

    added art descriptions

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,26 @@
 <template>
   <v-container class="text-center">
-    <v-img
-      src="/logo.png"
-      alt=""
-    ></v-img>
+    <v-img src="/logo.png" alt></v-img>
     <v-row>
-      <v-col
-        cols="6"
-        v-for="image in images"
-        v-bind:key="image"
-      >
-        <v-img :src="image">
-        </v-img>
+      <v-col cols="6" v-for="imageData in images" v-bind:key="imageData.image">
+        <v-img class="art-image" :src="imageData.image" @click="openDialog(imageData)"></v-img>
+        <v-dialog v-model="dialogOpen" max-width="800">
+          <v-card>
+            <v-img class="white--text" :src="currentDialog.image" max-height="500">
+              <v-card-title class="align-end fill-height">{{ currentDialog.title }}</v-card-title>
+            </v-img>
+            <v-card-text>{{ currentDialog.description }}</v-card-text>
+          </v-card>
+        </v-dialog>
       </v-col>
     </v-row>
     <nuxt-link to="/about">
-      <v-btn
-        fixed
-        dark
-        fab
-        bottom
-        right
-        outlined
-        color="pink lighten-4"
-      >
+      <v-btn fixed dark fab top right outlined color="pink lighten-4">
         <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
     </nuxt-link>
     <nuxt-link to="/contact">
-      <v-btn
-        fixed
-        dark
-        fab
-        bottom
-        left
-        outlined
-        color="pink lighten-4"
-      >
+      <v-btn fixed dark fab top left outlined color="pink lighten-4">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
     </nuxt-link>
@@ -44,8 +28,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
 
 export default {
   components: {
@@ -55,13 +39,49 @@ export default {
   data: () => {
     return {
       images: [
-        '/portfolio/1.jpg',
-        '/portfolio/2.jpg',
-        '/portfolio/3.jpg',
-        '/portfolio/4.jpg',
-        '/portfolio/5.jpg',
-      ]
+        {
+          image: "/portfolio/1.jpg",
+          title: "Art Title",
+          description: "Art Description"
+        },
+        {
+          image: "/portfolio/2.jpg",
+          title: "Art Title",
+          description: "Art Description"
+        },
+        {
+          image: "/portfolio/3.jpg",
+          title: "Art Title",
+          description: "Art Description"
+        },
+        {
+          image: "/portfolio/4.jpg",
+          title: "Art Title",
+          description: "Art Description"
+        },
+        {
+          image: "/portfolio/5.jpg",
+          title: "Art Title",
+          description: "Art Description"
+        }
+      ],
+      currentDialog: {},
+      dialogOpen: false
+    };
+  },
+  methods: {
+    openDialog(dialog) {
+      this.currentDialog = dialog;
+      this.dialogOpen = true;
     }
   }
-}
+};
 </script>
+
+<style scoped>
+.art-image:hover {
+  transition: 1000ms;
+  cursor: pointer;
+  filter: saturate(0);
+}
+</style>
